c2f176b8129a47b3ee54820f77987a15a037135a
Casey Litton <caseylitton@gmail.com>
2019-05-15 09:22:14 -0700

    testing 2

--- cloud-build/redis-conf.sh
+++ cloud-build/redis-conf.sh
@@ -10,3 +10,5 @@ cp /etc/redis/redis.conf /etc/redis/redis.conf.default
 sed -i -e 's/port 6379/port 9250/' /etc/redis/redis.conf
 # Allow remote connections
 sed -i -e 's/bind 127\.0\.0\.1/bind 0\.0\.0\.0/' /etc/redis/redis.conf
+# restart
+service redis-server restart
