21e20534c0df0c04c871d4983b2825b84a5c989e
Casey Litton <caseylitton@gmail.com>
2019-05-15 09:24:34 -0700

    testing 2

---
+++ cloud-build/postgres-install-93.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Setup elastic search config
+# root user
+# apt deps:
+#	elasticsearch
+
+# Set available java memory
+MEMGIGS=$(awk '/MemTotal/{printf "%%.0f", $2 / 1024**2}' /proc/meminfo)
+if [ "$MEMGIGS" -gt 32 ]
+then
+   echo "-Xms8g" >> /etc/elasticsearch/jvm.options
+   echo "-Xmx8g" >> /etc/elasticsearch/jvm.options
+elif [ "$MEMGIGS" -gt 12 ]
+then
+   echo "-Xms4g" >> /etc/elasticsearch/jvm.options
+   echo "-Xmx4g" >> /etc/elasticsearch/jvm.options
+else
+   echo "-Xms2g" >> /etc/elasticsearch/jvm.options
+   echo "-Xmx2g" >> /etc/elasticsearch/jvm.options
+   sysctl "vm.swappiness=1"
+   swapon /swapfile
+fi
+# not sure
+update-rc.d elasticsearch defaults
+# restart
+service elasticsearch restart
