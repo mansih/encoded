19456c298046cdb943c60f8feab828c2b0209db4
Casey Litton <caseylitton@gmail.com>
2019-05-15 08:57:36 -0700

    testing 2

---
+++ cloud-build/redis-conf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Setup redis config
+# root user
+
+# Backup default redis config
+- cp /etc/redis/redis.conf /etc/redis/redis.conf.default
+# Switch redis port
+- sed -i -e 's/port 6379/port %(REDIS_PORT)s/' /etc/redis/redis.conf
+# Allow remote connections
+- sed -i -e 's/bind 127\.0\.0\.1/bind 0\.0\.0\.0/' /etc/redis/redis.conf
