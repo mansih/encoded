869ed22dbd0e674f05c11eb7f7c360bd91f07b9d
Casey Litton <caseylitton@gmail.com>
2019-05-17 13:11:53 -0700

    fri test 1

--- cloud-build/encd-install.sh
+++ cloud-build/encd-install.sh
@@ -3,14 +3,20 @@
 # encoded user
 # apt deps:
 
+GIT_REPO="$1"
+GIT_BRANCH="$2"
+ROLE="$3"
+ES_IP="$4"
+ES_PORT="$5"
+
 mkdir /srv/encoded
 chown encoded:encoded /srv/encoded
 cd /srv/encoded
-sudo -u encoded git clone %(GIT_REPO)s . 
-sudo -u encoded git checkout -b %(GIT_BRANCH)s origin/%(GIT_BRANCH)s
+sudo -u encoded git clone "$GIT_REPO" . 
+sudo -u encoded git checkout -b "$GIT_BRANCH" origin/"$GIT_BRANCH"
 sudo pip3 install -U zc.buildout setuptools redis
 sudo -u encoded buildout bootstrap
-sudo -u encoded LANG=en_US.UTF-8 bin/buildout -c %(ROLE)s.cfg buildout:es-ip=%(ES_IP)s buildout:es-port=%(ES_PORT)s
+sudo -u encoded LANG=en_US.UTF-8 bin/buildout -c "$ROLE".cfg buildout:es-ip="$ES_IP" buildout:es-port="$ES_PORT"
 sudo -u encoded bin/aws s3 cp --recursive s3://encoded-conf-prod/.aws .aws
 until sudo -u postgres psql postgres -c ""; do sleep 10; done
 sudo -u encoded sh -c 'cat /dev/urandom | head -c 256 | base64 > session-secret.b64'
