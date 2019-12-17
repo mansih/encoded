d9ff4beaaac40226a57e83a0563a698ec9e338bc
Casey Litton <caseylitton@gmail.com>
2019-12-17 15:50:02 -0800

    update pre install encd

--- cloud-config/deploy-run-scripts/encd-preinstall.sh
+++ cloud-config/deploy-run-scripts/encd-preinstall.sh
@@ -7,15 +7,17 @@ GIT_REPO="$1"
 GIT_REMOTE="$2"
 GIT_BRANCH="$3"
 
-git_uri="$GIT_REMOTE/$GIT_BRANCH"
 
-encd_pybin='/srv/encoded/.pyvenv/bin'
+git_uri="$GIT_REMOTE/$GIT_BRANCH"
 encd_home='/srv/encoded'
 mkdir "$encd_home"
 chown encoded:encoded "$encd_home"
 cd "$encd_home"
-sudo -u encoded python3 -m venv .pyvenv
-sudo -u encoded "$encd_pybin/pip" install --upgrade pip zc.buildout setuptools redis
-sudo -u encoded git clone "$GIT_REPO" .
-sudo -u encoded git checkout -b "$GIT_BRANCH" "$git_uri"
-sudo -u encoded "$encd_pybin/pip" install -r requirements.txt
+sudo -H -u encoded git clone "$GIT_REPO" "$encd_home"
+sudo -H -u encoded git checkout -b "$GIT_BRANCH" "$git_uri"
+
+encd_venv="$encd_home/.pyvenv"
+sudo -H -u encoded python3 -m venv "$encd_venv"
+sourde "$encd_venv/bin/activate"
+sudo -H -u encoded "$encd_venv/bin/pip" install --upgrade pip zc.buildout setuptools redis
+sudo -H -u encoded "$encd_venv/bin/pip" install -r "$encd_home/requirements.txt"
